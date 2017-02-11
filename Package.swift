b056b12c451037625db26e172f5260b8a2245313
Guillaume Lessard <dhtnstff@gmail.com>
2017-02-10 18:13:24 -0700

    update dependency version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
 	name: "syncprint",
 	dependencies: [
-    .Package(url: "https://github.com/glessard/swift-atomics.git", majorVersion: 2),
+    .Package(url: "https://github.com/glessard/swift-atomics.git", majorVersion: 3),
   ]
 )

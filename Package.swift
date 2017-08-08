21b38428dbebd61216e14b19ec4a48c9c05e90f5
Guillaume Lessard <dhtnstff@gmail.com>
2017-08-08 00:27:52 -0600

    update package description to Swift 4 format

--- Package.swift
+++ Package.swift
@@ -1,8 +1,30 @@
+// swift-tools-version:4.0
+
 import PackageDescription
+
+#if swift(>=4.0)
 
 let package = Package(
 	name: "syncprint",
+	products: [
+    .library(name: "syncprint", type: .static, targets: ["syncprint"]),
+  ],
 	dependencies: [
+    .package(url: "https://github.com/glessard/swift-atomics.git", from: "3.0.0"),
+  ],
+  targets: [
+    .target(name: "syncprint", dependencies: ["Atomics"], path: "Sources"),
+    .testTarget(name: "syncprintTests", dependencies: ["syncprint"])
+  ]
+)
+
+#else
+
+let package = Package(
+  name: "syncprint",
+  dependencies: [
     .Package(url: "https://github.com/glessard/swift-atomics.git", majorVersion: 3),
   ]
 )
+
+#endif
